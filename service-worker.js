4b6126337941811e37dc046415435e1b8536c449
Dr. Zoom <141271563+ProfessorZoom45@users.noreply.github.com>
2026-04-29 08:35:43 -0400

    Update service worker from website repo zip

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,5 @@
-const CACHE_NAME = 'ctf-app-shell-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './assets/css/app.css',
-  './assets/js/app.js'
-];
-
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>{
-      if(k !== CACHE_NAME) return caches.delete(k);
-    })))
-  );
-});
-
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+const CACHE='ctf-browser-beta-pwa-2';
+const ASSETS=['./','./index.html','./cards.html','./deckbuilder.html','./rules.html','./glossary.html','./play.html','./submit.html','./About_Us.html','./beginners-guide.html','./privacy-policy.html','./play_inline.js','./assets/js/ctf-config.js','./assets/js/data.js','./assets/js/deck-utils.js','./assets/js/engine.js','./manifest.json','./icon-ctf.svg','./images/index-promo.jpg','./images/about-guide.jpg','./assets/js/card-image-map.js','./images/placeholder.gif'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET') return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{}); return resp;}).catch(()=>caches.match('./index.html'))));});
